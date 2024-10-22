1efe2f7614c4a3aef16a87b808cd40baaa9f9efa
phautena lab <phautena@made-f0cr6s6.paris.europe.42.school>
2024-10-22 13:20:48 +0200

    * Not much

---
+++ includes/philo.h
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: phautena <phautena@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/22 12:54:52 by phautena          #+#    #+#             */
+/*   Updated: 2024/10/22 13:14:16 by phautena         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <stdbool.h>
+# include <sys/time.h>
+# include <pthread.h>
+#include <limits.h>
+
+typedef pthread_mutex_t	t_mtx;
+typedef struct s_table t_table;
+
+typedef struct s_fork
+{
+	t_mtx	fork;
+	int		fork_id;
+}	t_fork;
+
+typedef struct s_philo
+{
+	int			id;
+	long		meals_counter;
+	bool		full;
+	long		last_meal_time;
+	t_fork		*left_fork;
+	t_fork		*right_fork;
+	pthread_t	thread_id;
+	t_table		*table;
+}	t_philo;
+
+typedef struct s_table
+{
+	long	philo_nbr;
+	long	time_to_die;
+	long	time_to_eat;
+	long	time_to_sleep;
+	long	nbr_limit_meals;
+	long	start_simulation;
+	bool	end_simulation;
+	t_fork	*fork;
+	t_philo	*philos;
+}	t_table;
+
+#endif
